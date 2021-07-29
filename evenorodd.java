6ef89d9bb57a4516ff6c1eb667d4662669269c72
Farzeen <farzeenkhan1412@gmail.com>
2021-07-29 20:32:32 +0530

    second

--- evenorodd.java
+++ evenorodd.java
@@ -1,17 +1,11 @@
 package day5;
+
 import java.util.Scanner;
 
 public class evenorodd {
 	public static void main(String[] args) {
+ System.out.println("welcome");
+	}
+	
 
-	 Scanner reader = new Scanner(System.in);
-
-     System.out.print("Enter a number: ");
-     int num = reader.nextInt();
-
-     if(num % 2 == 0)
-         System.out.println(num + " is even");
-     else
-         System.out.println(num + " is odd");
-}
 }
