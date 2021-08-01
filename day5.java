86db94ac72dcd104b21e30f877fd47412972f1b1
Farzeen <farzeenkhan1412@gmail.com>
2021-08-02 00:27:50 +0530

    qnr

--- day5.java
+++ day5.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class day5 {
 
 	public static void main(String[] args) {
-		
-		{
-			double n=10,i;
-			double sum=0;	
-			for( i=1;i<=n;i++)
-			{
-				sum=sum+(1/i);
-			}
-			System.out.println("Harmonic value = "+sum);
-			}
-	}
-}
+		Scanner a = new Scanner(System.in);
+				System.out.println("enter dividend :");
+		int dividend = a.nextInt();
+				Scanner b = new Scanner(System.in);
+				System.out.println("enter divisor :");
+		int divisor = b.nextInt();
+				
+		 int quotient = dividend / divisor;
+		    int remainder = dividend % divisor;
+
+		    System.out.println("Quotient = " + quotient);
+		    System.out.println("Remainder = " + remainder);
+		  }
+		}
