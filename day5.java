9c6a6ed1905c67b5ff6105228d9e2a34abcb9d9a
Farzeen <farzeenkhan1412@gmail.com>
2021-08-02 00:17:44 +0530

    harmonic

--- day5.java
+++ day5.java
@@ -1,27 +1,19 @@
 package day5;
-
-import java.util.Random;
+import java.util.Scanner;
 
 
 public class day5 {
 
 	public static void main(String[] args) {
 		
-		float heads = 0;
-		float tails = 0;
-		for (int i = 0;i < 50;i++) {
-			Random r = new Random();
-			int flip = r.nextInt(2);
-			if (flip == 0)
-				heads = heads+1;
-			else
-				tails = tails+1;
-				
-		}
-		System.out.println("heads won:" + heads);
-		System.out.println("tails won:" + tails);
-		System.out.printf("tails percentage is : %.2f\n", (tails / (heads + tails)) *100);
-		System.out.printf("heads percentage is : %.2f\n", (heads / (heads + tails)) *100);
-		}
-	
+		{
+			double n=10,i;
+			double sum=0;	
+			for( i=1;i<=n;i++)
+			{
+				sum=sum+(1/i);
+			}
+			System.out.println("Harmonic value = "+sum);
+			}
+	}
 }
